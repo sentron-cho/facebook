2d57e6233d93b4731491e5df255a87c46ded61b4
sentron-cho <47094028+sentron-cho@users.noreply.github.com>
2022-11-19 09:19:42 +0900

    초기 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
+import Login from './view/Login.jsx'
+import Home from './view/Home.jsx'
+import Regist from './view/Regist.jsx'
+import Identify from './view/Identify.jsx'
+
+import Video from './view/Video.jsx'
+import People from './view/People.jsx'
+import Game from './view/Game.jsx'
+
+import './css/default.scss'
+import './css/react.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {/* 헤더에서 클릭된 URL을 찾아 매칭되는 화면을 그리기 위한 라우터를 설정한다. */}
+      <main>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/regist" element={<Regist />} />
+          <Route path="/identify" element={<Identify />} />
+          <Route path="/video" element={<Video />} />
+          <Route path="/people" element={<People />} />
+          <Route path="/game" element={<Game />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
